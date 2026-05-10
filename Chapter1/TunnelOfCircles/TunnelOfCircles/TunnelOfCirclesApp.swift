ce9da4f79daa408b08f8fa48533d592b31a3e960
jinnarajin <unijinramyeon@knu.ac.kr>
2026-05-10 22:43:21 +0900

    Feat: chapter 1 tutorial

---
+++ Chapter1/TunnelOfCircles/TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,18 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by Seojin Lee on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowResizability(.contentSize)
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# visionOS-Playground-Tutorials
